7c440cd70793a989b6d376122b7b59752e9b581e
Baptiste Cainjo <cainjo.baptiste@orange.fr>
2025-09-27 15:26:14 +0200

    Update greeting message in HelloController to French

--- server/src/hello/hello.controller.ts
+++ server/src/hello/hello.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HelloController {
   @Get() // GET /hello
   getHello(): string {
-    return 'Hello World';
+    return 'Bonjour je suis baptiste';
   }
 }
