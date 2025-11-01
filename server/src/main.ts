ee2e892ee88c133bc7e7d114dee258402ce6168b
Baptiste Cainjo <cainjo.baptiste@orange.fr>
2025-11-01 17:09:07 +0100

    Fix CORS origin URL by removing trailing slash

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://match-three-steel.vercel.app/'],
+    origin: ['https://match-three-steel.vercel.app'],
     credentials: true,
   });
 
